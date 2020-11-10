e5b0a60df45ceaaac8131b9860d3a1c8728836ec
willshuffy <saputrawilly26@gmail.com>
2020-11-11 03:15:51 +0700

    adding intent with back stack function

--- app/src/main/java/com/willshuffyproject/mydeepnavigation/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/mydeepnavigation/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.willshuffyproject.mydeepnavigation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_open_detail.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_open_detail){
+            val detailIntent = Intent(this@MainActivity, DetailActivity::class.java)
+            detailIntent.putExtra(DetailActivity.EXTRA_TITLE, getString(R.string.detail_title))
+            detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, getString(R.string.detail_message))
+            startActivity(detailIntent)
+        }
     }
 }
